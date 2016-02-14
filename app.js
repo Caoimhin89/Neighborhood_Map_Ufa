5f25fd26ee7d867345abdbb30761713c3d593909
Kevin Glick <kevinglick@Kevins-MacBook-Air.local>
2016-02-14 15:51:44 -0500

    add additional descriptions to POI

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ function initAutocomplete() {
     });
 
     var siteInfo = {
-      aviationUni:"This is the Aviation University.",
-      guestYard:"This is the Guest Yard.",
-      kashPark:"This is Kashkadan Park.",
-      yakPark:"This is Park Yakutova.",
-      lala:"This is the Lala Tulpan Mosque.",
-      cityCouncil:"This is the City Council.",
-      bashTheater:"This is the Bashkir Drama Theater.",
-      salavat:"This is Park Salavat Ulayev.",
-      tatTheater:"This is the Tatar Drama Theater.",
-      victory:"This is Victory Park.",
-      congress:"This is the Congress Hall.",
-      whiteHouse:"This is the White House.",
-      friendship:"This is the Friendship Monument.",
-      bgu:"This is Bashkir State University.",
-      ballet:"This is the Ballet & Opera Theater."
+      aviationUni:"This is the Aviation University. It was founded in 1932 and is now one of the leading universities in Russia.",
+      guestYard:"This is the Guest Yard. At its center is a modestly-sized shopping mall which sits beside a popular scenic park with statues of famous locals and surrounded by restaurants, hotels, and various company headquarters.",
+      kashPark:"This is Kashkadan Park. Built in 2002, the park sits on the shoreline of lake Kashkadan and provides a place of leisure and entertainment for locals and tourists.",
+      yakPark:"This is Park Yakutova. The park is situated in the center of the city. In the summer it has festival rides and attractions and is frequently used for holidays and cultural events. It is one of the most popular parks in Ufa.",
+      lala:"This is the Lala Tulpan Mosque. One of the most iconic buildings in Ufa, it is located just outside Victory Park in the northern part of the city. One of the largest mosques in Russia, it can hold up to a thousand worshippers and stands at 53 meters in height. It's twin minarets are designed to resemble blooming tulips.",
+      cityCouncil:"This is the City Council building. It is located along the city's main street and just across from one of the most prominent parks in the city. It is the home of the city's legislature.",
+      bashTheater:"This is the Bashkir Drama Theater. It holds various cultural events such as musical concerts, live-action plays, and poetry readings all in the Bashkir language.",
+      salavat:"This is Park Salavat Ulayev. This is the main and most popular park in the city. It is situated just a short walk from the seat of government of the Bashkir Republic, the television station, and the main university. It features a statue of its namesake, Salavat Ulayev, a Bashkir folk hero and one of the leaders of the Pugachev rebellion.",
+      tatTheater:"This is the Tatar Drama Theater. It hosts a wide array of cultural events and theatrical performances in the Tatar language.",
+      victory:"This is Victory Park. It was built to comemorate the soviet triumph over Nazi Germany in the Second World War.",
+      congress:"This is the Congress Hall. It is used to host a wide number of important events for the city of Ufa. It was recently used to host the BRICS economic summit.",
+      whiteHouse:"This is the White House. It is the seat of government of the Republic of Bashkortostan.",
+      friendship:"This is the Friendship Monument. It was built in 1965 to celebrate the 400-year anniversary of Bashkiria's accession to the Russian Empire and the friendship between the Russian and Bashkir peoples.",
+      bgu:"This is Bashkir State University. It is the main university in Ufa and is situated on the main street. This is where I studied from September, 2012 through June, 2013.",
+      ballet:"This is the Ballet & Opera Theater. Situated in the center of the city, it hosts many classical and contemporary Russian and European spectacles."
     };
 
 //Set up point object to receive all info about map locations
